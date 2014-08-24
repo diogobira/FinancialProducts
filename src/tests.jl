24cef151a54232d07b5803f99fd6529ad91d7ab8
Diogo Gobira <diogobira@gmail.com>
2014-08-23 22:50:44 -0300

    alguns testes...

--- src/tests.jl
+++ src/tests.jl
@@ -1,20 +1,58 @@
 
+### Dependencies
+using DataFrames
+
 ### Basic Types
 require("basic_types.jl")
+require("basic_operations.jl")
 
 ### Products
 require("bullet.jl")
 require("sac.jl")
 require("stock.jl")
 
-### Stubs for some indexes (trimestral)
-petr4_idx = [ (i,25*(1+0.15)^(i/12)) for i=1:3:1200]
-selic_idx = [ (i,1 + rand(0.0:0.01:0.1)) for i=1:3:1200]
-tjlp_idx = [ (i,1 + rand(0.0:0.01:0.1)) for i=1:3:1200]
-
 ###
-o = stock(1000.0, 0.05, 3, "PETR4")
-b = bullet(1000.0, 60, "SELIC")
-w = sac(1000.0, 60, 0.1, "TJLP")
-#projectCashFlows(o)
+#o = stock(1000.0, 0.05, 3, "PETR4")
+#b = bullet(1000.0, 60, "SELIC")
+#w = sac(1000.0, 60, 0.1, "TJLP")
+
+
+### Tests ###
+
+#Create an array of 1000 financial contracts
+contracts = Any[]
+for i=1:50
+	push!(contracts, bullet(1000.0, 60, "SELIC"))
+	push!(contracts, sac(1000.0, 60, 0.1, "TJLP"))
+end
+
+#Initialize the an array with cashFlows of pre-existent contracts
+cashFlows = cashFlow[]
+mtms = Float64[]
+@time for i=1:length(contracts)
+	cashFlows = vcat(cashFlows, projectCashFlow(contracts[i]))	 
+end
+
+#Loop on simulation horizon, creating new contracts and computing the mtm
+@time for j=1:60
+
+	#create new 20 contracts
+	newcontracts = Any[]
+	for j=1:15
+		push!(newcontracts, bullet(1000.0, 60, "SELIC"))
+		push!(newcontracts, sac(1000.0, 60, 0.1, "TJLP"))
+	end
+	
+	#update the cashFlows array
+	for i=1:length(newcontracts)
+		cashFlows = vcat(cashFlows, projectCashFlow(newcontracts[i]))	 
+	end
+
+	for i=1:length(cashFlows)
+		d = getProcessFromIndexName("TJLP", discountProcess)
+		mtms = vcat(mtms, mtm(cashFlows[i][:onDomestic], d))
+	end
+
+end
+
 
